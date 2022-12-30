39158b0710ba1f68a30c4a3c258a5e5c3993ca1c
Tas <tasqur9@gmail.com>
2022-12-30 11:52:45 +0100

    RepositoryPattern

---
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BulkyBook.DataAccess;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+}
+
